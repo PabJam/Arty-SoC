49fc1d2b24f67af0f23c0704795031528470badc
PabJam <jamin.pablo@googlemail.com>
2025-11-05 12:13:13 +0100

    Added nuget package and set up simple serial port

--- ComPortUI/MainWindow.xaml.cs
+++ ComPortUI/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO.Ports;
 
 namespace ComPortUI
 {
@@ -16,9 +17,29 @@ namespace ComPortUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Hard coded constants (for now)
+        const string portName = "COM7";
+        const int baudRate = 9600;
+        const Parity parity = Parity.None;
+        const int dataBits = 8;
+        const StopBits stopBits = StopBits.One;
+
+
+        private SerialPort serialPort;
         public MainWindow()
         {
             InitializeComponent();
+            serialPort = new SerialPort(portName, baudRate, parity, dataBits, stopBits);
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialDataRecievedHandler);
+        }
+
+        private static void SerialDataRecievedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+
+            //Read all available data
+            string data = sp.ReadExisting();
+            //TODO write to text window
         }
     }
 }
