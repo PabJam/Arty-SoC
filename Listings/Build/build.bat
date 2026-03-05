f61414826a92dffa17a4c253d045d5313b317d25
PabJam <jamin.pablo@googlemail.com>
2026-03-05 12:15:47 +0100

    Added Fibonacci Test

--- Listings/Build/build.bat
+++ Listings/Build/build.bat
@@ -1,31 +1,31 @@
 @echo off
-riscv-none-elf-gcc.exe -march=rv32i -mabi=ilp32 -march=rv32i -mabi=ilp32 -march=rv32i -mabi=ilp32 -march=rv32i -mabi=ilp32 -march=rv32i -mabi=ilp32 -ID:\Projekte\Arty\Listings\C\Libs -c D:\Projekte\Arty\Listings\Build\startup.s -o D:\Projekte\Arty\Listings\Build\startup.o
+riscv-none-elf-gcc.exe -march=rv32i -mabi=ilp32 -march=rv32i -mabi=ilp32 -march=rv32i -mabi=ilp32 -march=rv32i -mabi=ilp32 -march=rv32i -mabi=ilp32 -march=rv32i -mabi=ilp32 -march=rv32i -mabi=ilp32 -ID:\Projekte\Arty\Listings\C\Libs -c D:\Projekte\Arty\Listings\Build\startup.s -o D:\Projekte\Arty\Listings\Build\startup.o
 if %errorlevel% neq 0 (
     echo startup.s compilation failed!  
     exit /b 1
 )
-riscv-none-elf-gcc.exe -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -O2 -ID:\Projekte\Arty\Listings\C\Libs -c D:\Projekte\Arty\Listings\C\I2C_BadApple.c -o D:\Projekte\Arty\Listings\Build\I2C_BadApple.o
+riscv-none-elf-gcc.exe -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -O2 -ID:\Projekte\Arty\Listings\C\Libs -c D:\Projekte\Arty\Listings\C\Fibonacci.c -o D:\Projekte\Arty\Listings\Build\Fibonacci.o
 if %errorlevel% neq 0 (
-    echo I2C_BadApple.c compilation failed!  
+    echo Fibonacci.c compilation failed!  
     exit /b 1
 )
-riscv-none-elf-gcc.exe -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -O2 -ID:\Projekte\Arty\Listings\C\Libs -c D:\Projekte\Arty\Listings\C\Libs\Peripherals.c -o D:\Projekte\Arty\Listings\Build\Peripherals.o
+riscv-none-elf-gcc.exe -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -O2 -ID:\Projekte\Arty\Listings\C\Libs -c D:\Projekte\Arty\Listings\C\Libs\Peripherals.c -o D:\Projekte\Arty\Listings\Build\Peripherals.o
 if %errorlevel% neq 0 (
     echo Peripherals.c compilation failed!  
     exit /b 1
 )
-riscv-none-elf-gcc.exe -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -O2 -ID:\Projekte\Arty\Listings\C\Libs -c D:\Projekte\Arty\Listings\C\Libs\Utils.c -o D:\Projekte\Arty\Listings\Build\Utils.o
+riscv-none-elf-gcc.exe -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -march=rv32i -mabi=ilp32 -ffreestanding -nostdlib -O2 -ID:\Projekte\Arty\Listings\C\Libs -c D:\Projekte\Arty\Listings\C\Libs\Utils.c -o D:\Projekte\Arty\Listings\Build\Utils.o
 if %errorlevel% neq 0 (
     echo Utils.c compilation failed!  
     exit /b 1
 )
-riscv-none-elf-gcc.exe -march=rv32i -mabi=ilp32 -nostdlib -march=rv32i -mabi=ilp32 -nostdlib -march=rv32i -mabi=ilp32 -nostdlib -march=rv32i -mabi=ilp32 -nostdlib -T D:\Projekte\Arty\Listings\Build\linker.ld -o D:\Projekte\Arty\Listings\Build\I2C_BadApple.elf D:\Projekte\Arty\Listings\Build\startup.o D:\Projekte\Arty\Listings\Build\I2C_BadApple.o D:\Projekte\Arty\Listings\Build\Peripherals.o D:\Projekte\Arty\Listings\Build\Utils.o -lgcc -lgcc -lgcc -lgcc
+riscv-none-elf-gcc.exe -march=rv32i -mabi=ilp32 -nostdlib -march=rv32i -mabi=ilp32 -nostdlib -march=rv32i -mabi=ilp32 -nostdlib -march=rv32i -mabi=ilp32 -nostdlib -march=rv32i -mabi=ilp32 -nostdlib -march=rv32i -mabi=ilp32 -nostdlib -T D:\Projekte\Arty\Listings\Build\linker.ld -o D:\Projekte\Arty\Listings\Build\Fibonacci.elf D:\Projekte\Arty\Listings\Build\startup.o D:\Projekte\Arty\Listings\Build\Fibonacci.o D:\Projekte\Arty\Listings\Build\Peripherals.o D:\Projekte\Arty\Listings\Build\Utils.o -lgcc -lgcc -lgcc -lgcc -lgcc -lgcc
 if %errorlevel% neq 0 (
     echo Linking failed!
     exit /b 1
 )
-riscv-none-elf-objcopy.exe -O binary D:\Projekte\Arty\Listings\Build\I2C_BadApple.elf D:\Projekte\Arty\Listings\Build\I2C_BadApple.bin
+riscv-none-elf-objcopy.exe -O binary D:\Projekte\Arty\Listings\Build\Fibonacci.elf D:\Projekte\Arty\Listings\Build\Fibonacci.bin
 echo ========================================
 echo Build complete!
 echo ========================================
-riscv-none-elf-size.exe D:\Projekte\Arty\Listings\Build\I2C_BadApple.elf
+riscv-none-elf-size.exe D:\Projekte\Arty\Listings\Build\Fibonacci.elf
---
+++ Listings/C/Fibonacci.c
@@ -0,0 +1,36 @@
+#include ".\Libs\Peripherals.h"
+
+int Fibonacci(unsigned int n);
+
+// Main function - called by startup.s
+int main(void) {
+    print("Calculating Fibonacci numbers for 10 seconds\r\n");
+    
+    unsigned int timerStart = read_timer();
+    unsigned int timer = 0;
+    unsigned int n = 0;
+    unsigned int result = 0;
+
+    while (1)
+    {
+        timer = read_timer();
+        if (timer - timerStart > 1000000000) { break; } // 100MHZ * 1sec
+        n++;
+        result = Fibonacci(n);
+    }
+
+    print("Calculated ");
+    print_int(n);
+    print(" (");
+    print_int(result);
+    print(") Fibonacci numbers in 10 seconds\r\n");
+
+    return 0;
+}
+
+int Fibonacci(unsigned int n)
+{
+    if (n == 0) { return 0; }
+    else if (n == 1) { return 1; }
+    else { return Fibonacci(n - 2) + Fibonacci(n - 1); }
+}
